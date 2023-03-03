2dff243ec94761bd047fe5dfdce4ee468fc8bbd2
Michu-dev <michalciesielski2000@gmail.com>
2023-03-03 16:33:48 +0100

    Add ZMIO and Big Data initial folders

---
+++ Big Data/Esper-projekt/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = new InputStream();
+        inputStream.generuj();
+    }
+}
